9cee04c3a46a7106a1c50896e8252ef66175e01d
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2018-11-11 20:52:32 -0500

    added procfile to heroku, to start apache2 and modified settings.php to include env vars from heroku

---
+++ web/sites/default/settings.php
@@ -0,0 +1,33 @@
+<?php
+
+$settings['hash_salt'] = getenv('HASH_SALT');
+
+$settings['update_free_access'] = FALSE;
+
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+
+$settings['entity_update_batch_size'] = 50;
+
+$config_directories['sync'] = '../config/sync';
+
+// Read DB settings.
+$db_settings = parse_url(getenv('DATABASE_URL'));
+$databases['default']['default'] = array (
+  'database' => ltrim($db_settings['path'], '/'),
+  'username' => $db_settings['user'],
+  'password' => $db_settings['pass'],
+  'prefix' => '',
+  'host' => $db_settings['host'],
+  'port' => $db_settings['port'],
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\pgsql',
+  'driver' => 'pgsql',
+);
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
